632d100f8dbecff37d15e8d79cd88de9465c41d3
Lozamded <dhiguerafernandez@gmail.com>
2024-04-10 10:09:05 -0400

    MOD: Mathjax latex reload
    ADD: Color Change
    
    FIX: FIX identation

--- eolquestion/static/js/src/studio.js
+++ eolquestion/static/js/src/studio.js
@@ -1,8 +1,23 @@
 function EolQuestionStudioXBlock(runtime, element) {
 
     var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
-  
-    $(element).find('.save-button').bind('click', function(e) {
+
+    $(element).find('select[name=theme]').change(function () {
+      var theme = $(this).val();
+      var colorInput = $(element).find('input[name=color]');
+
+      if (theme === "SumaySigue") {
+        colorInput.val('#e71f24');
+      } else if (theme === "Media") {
+        colorInput.val('#612871');
+      } else if (theme === "RedFid") {
+        colorInput.val('#0C8AA8');
+      } else if (theme === "Didactica") {
+        colorInput.val('#e71f24');
+      }
+    });
+
+    $(element).find('.save-button').bind('click', function (e) {
       var form_data = new FormData();
       var display_name = $(element).find('input[name=display_name]').val();
       var type = $(element).find('select[name=type]').val();
@@ -21,11 +36,11 @@ function EolQuestionStudioXBlock(runtime, element) {
       form_data.append('theme', theme);
       form_data.append('color', color);
       form_data.append('idspecific', idspecific);
-      
+
       if ($.isFunction(runtime.notify)) {
         runtime.notify('save', {state: 'start'});
       }
-  
+
       $.ajax({
         url: handlerUrl,
         dataType: 'text',
@@ -41,13 +56,10 @@ function EolQuestionStudioXBlock(runtime, element) {
         }
       });
       e.preventDefault();
-  
     });
-  
-    $(element).find('.cancel-button').bind('click', function(e) {
+    
+    $(element).find('.cancel-button').bind('click', function (e) {
       runtime.notify('cancel', {});
       e.preventDefault();
     });
-  
-  }
-  
+}
